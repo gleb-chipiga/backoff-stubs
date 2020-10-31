42083d17b2a5e118bfae10c7194f710161b76233
Gleb Chipiga <gleb.chipiga+git@pm.me>
2020-11-01 01:49:18 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,25 @@ setup(
     name='backoff-stubs',
     author='Gleb Chipiga',
     version='1.10.0',
+    url='https://github.com/gleb-chipiga/backoff-stubs',
+    repository='https://github.com/gleb-chipiga/backoff-stubs',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 5 - Production/Stable',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    license='MIT',
+    keywords=['retry', 'backoff', 'decorators', 'stubs', 'mypy'],
     packages=['backoff-stubs'],
-    package_data={'backoff-stubs': ['__init__.pyi']}
+    package_data={'backoff-stubs': ['__init__.pyi']},
+    python_requires='>=3.8'
 )
