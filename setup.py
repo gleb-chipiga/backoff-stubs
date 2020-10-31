1b127485963313b8aa45ae536653876c430ed68d
Gleb Chipiga <gleb.chipiga+git@pm.me>
2020-11-01 00:57:32 +0300

    Add setup.py and LICENSE file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='backoff-stubs',
+    author='Gleb Chipiga',
+    version='1.10.0',
+    packages=['backoff-stubs'],
+    package_data={'backoff-stubs': ['__init__.pyi']}
+)
